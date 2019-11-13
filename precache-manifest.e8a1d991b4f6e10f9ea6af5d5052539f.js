c481353b3df3cc87c48884f9f2435d7735dcc76b
Ash Todd <ashztodd@gmail.com>
2019-11-13 15:24:25 -0800

    Updates

---
+++ precache-manifest.e8a1d991b4f6e10f9ea6af5d5052539f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3992a3df85b6509d4b13a51fc4d0ff74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eefd68f657a58c10c12",
+    "url": "/static/css/main.fd03b0a4.chunk.css"
+  },
+  {
+    "revision": "1a3fea4612c72df52a06",
+    "url": "/static/js/2.d7d7c697.chunk.js"
+  },
+  {
+    "revision": "9eefd68f657a58c10c12",
+    "url": "/static/js/main.b6d6f980.chunk.js"
+  },
+  {
+    "revision": "a6332d28dc89f230c7c8",
+    "url": "/static/js/runtime-main.c087ecd8.js"
+  }
+]);
