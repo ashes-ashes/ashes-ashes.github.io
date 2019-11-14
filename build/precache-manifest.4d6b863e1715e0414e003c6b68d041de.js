912dd68c79b51b5e28e23cef09b03b8a8596a7d7
Ash Todd <ashztodd@gmail.com>
2019-11-14 15:09:03 -0800

    Added about skills section

--- build/precache-manifest.4d6b863e1715e0414e003c6b68d041de.js
+++ build/precache-manifest.4d6b863e1715e0414e003c6b68d041de.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d65d3088ec448a3caa5bd140e69ffd7a",
+    "revision": "42a424c83270ec0c21f7786da8afbb7f",
     "url": "/index.html"
   },
   {
-    "revision": "ebeb327e42560e6d5fb3",
-    "url": "/static/css/main.1d1f5e31.chunk.css"
+    "revision": "0743be90ac2796cf8c54",
+    "url": "/static/css/main.15497c9f.chunk.css"
   },
   {
     "revision": "1a3fea4612c72df52a06",
     "url": "/static/js/2.d7d7c697.chunk.js"
   },
   {
-    "revision": "ebeb327e42560e6d5fb3",
+    "revision": "0743be90ac2796cf8c54",
     "url": "/static/js/main.0857c2d8.chunk.js"
   },
   {
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,48 @@ export default function About() {
     return (
         <div className="about-container">
             <h3>ash todd • san francisco • software developer</h3>
-            <p>lorem ipsums</p>
+            <h4>skills</h4>
+            <ul className="language list">
+                <li>
+                    <h5>Ruby</h5>
+                    <ul className="skill list">
+                        <li>Rails</li>
+                        <li>Action Cable</li>
+                        <li>Active Storage</li>
+                    </ul>
+                </li>
+                <li>
+                    <h5>JavaScript</h5>
+                    <ul className="skill list">
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>JQuery</li>
+                        <li>Vanilla DOM Manipulation</li>
+                        <li>AJAX</li>
+                    </ul>
+                </li>
+                <li>
+                    <h5>CSS3</h5>
+                    <ul className="skill list">
+                        <li>SASS</li>
+                    </ul>
+                </li>
+                <li>
+                    <h5>HTML5</h5>
+                    <ul className="skill list">
+                        <li>Canvas</li>
+                    </ul>
+                </li>
+                <li>
+                    <h5>...etc.</h5>
+                    <ul className="skill list">
+                        <li>SQL</li>
+                        <li>PostgreSQL</li>
+                        <li>AWS (Amazon Web Services)</li>
+                        <li>UI/UX Design</li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     )
 }
