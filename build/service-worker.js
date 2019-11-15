1d9bcdfb0c7cb0b529723312a422e3c96e352b5a
Ash Todd <ashztodd@gmail.com>
2019-11-15 14:45:19 -0800

    Slightly better mobile support

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2aa07f7d00f3d1bd58dd763bd1852f43.js"
+  "/precache-manifest.28d9d094d469370c7ee25d49c7e44210.js"
 );
 
 self.addEventListener('message', (event) => {
