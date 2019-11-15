22d6cddecab60787a20047a8857cd0082cc74c4b
Ash Todd <ashztodd@gmail.com>
2019-11-15 12:35:12 -0800

    Added background

--- build/precache-manifest.36e360ff7525394b3734bb8e9cdf4288.js
+++ build/precache-manifest.36e360ff7525394b3734bb8e9cdf4288.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42a424c83270ec0c21f7786da8afbb7f",
+    "revision": "6b0ea80b1f53fdfd3c27582caaa6235f",
     "url": "/index.html"
   },
   {
-    "revision": "0743be90ac2796cf8c54",
-    "url": "/static/css/main.15497c9f.chunk.css"
+    "revision": "6d355cf21889755ed135",
+    "url": "/static/css/main.3ffa071a.chunk.css"
   },
   {
     "revision": "1a3fea4612c72df52a06",
     "url": "/static/js/2.d7d7c697.chunk.js"
   },
   {
-    "revision": "0743be90ac2796cf8c54",
-    "url": "/static/js/main.0857c2d8.chunk.js"
+    "revision": "6d355cf21889755ed135",
+    "url": "/static/js/main.9559161e.chunk.js"
   },
   {
     "revision": "a6332d28dc89f230c7c8",
