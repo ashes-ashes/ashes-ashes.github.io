c5638be01d280a6e9cf2280baf2c54142a86939a
Ash Todd <ashztodd@gmail.com>
2020-01-22 17:06:08 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.acd53d839e623edce4049c1140de1d1c.js"
+  "/precache-manifest.6e1a03ff85bbf8ec7b867182beaa406c.js"
 );
 
 self.addEventListener('message', (event) => {
