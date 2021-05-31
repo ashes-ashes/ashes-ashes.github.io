73e5e6adef31d88afe22fbf7955563029f56beb8
ash todd <52134597+ashes-ashes@users.noreply.github.com>
2021-05-30 19:09:39 -0700

    Updates

--- precache-manifest.14060f5575500204a0fc1000ba5c6955.js
+++ precache-manifest.14060f5575500204a0fc1000ba5c6955.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f01290bf9db68f66a379445f896bd12",
+    "revision": "e749390a527ab847a307f0d595bc6994",
     "url": "/index.html"
   },
   {
-    "revision": "32c4105aa988cd5de57f",
-    "url": "/static/css/main.843ea0ff.chunk.css"
+    "revision": "085367054a19b404ea34",
+    "url": "/static/css/main.549cd31e.chunk.css"
   },
   {
     "revision": "1a3fea4612c72df52a06",
     "url": "/static/js/2.d7d7c697.chunk.js"
   },
   {
-    "revision": "32c4105aa988cd5de57f",
-    "url": "/static/js/main.0884e8ba.chunk.js"
+    "revision": "085367054a19b404ea34",
+    "url": "/static/js/main.226911cc.chunk.js"
   },
   {
     "revision": "a6332d28dc89f230c7c8",
